bfbc02d4c1435a4be9355208c50fe9a308d7e782
danilo <danilo@danilos-iMac-2.local>
2018-06-06 21:03:32 +0200

    Redux setup

---
+++ src/redux/index.js
@@ -0,0 +1,28 @@
+import { combineReducers, compose, createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger'
+import createSagaMiddleware from 'redux-saga';
+import promiseMiddleware from '../utils/promiseMiddleware';
+import home from './home';
+
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [promiseMiddleware, sagaMiddleware];
+
+middleware.push(createLogger());
+
+const createStoreWithMiddleware = compose(
+  applyMiddleware(
+    ...middleware,
+  ),
+)(createStore);
+
+const create = (...args) => {
+  const store = createStoreWithMiddleware(
+    combineReducers({
+      home
+    }),
+    ...args,
+  );
+  return store;
+};
+
+export default create;
